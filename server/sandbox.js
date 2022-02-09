2c1d9afa27ab9fcda3a36742d9dd393742e7b688
Seb987 <liaosebastien@DESKTOP-IQ1KTIP.localdomain>
2022-02-09 11:39:15 +0100

    Scrap all products from adresseparis

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,15 +1,17 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
-const montlimart = require('./sources/montlimart.js')
+const montlimart = require('./sources/montlimart.js');
+const adresseparis= require('./sources/adresseparis');
 const fs = require('fs');
 
 
 async function sandbox (eshop) {
   try {
     //console.log(`🕵️‍♀️  browsing ${eshop} source`);
-
-    const products = await montlimart.scrape(eshop);
-    console.log(products);
+    const products_dedicatedBrand = await dedicatedbrand.scrape(eshop);
+    const products_montlimart = await montlimart.scrape(eshop);
+    const products_adresseParis = await adresseparis.scrape(eshop)
+    console.log(products_montlimart);
     console.log('done');
   /*
     try {
