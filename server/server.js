985cb5d79d23428e40707f4591d732c19765711f
Seb987 <liaosebastien@DESKTOP-IQ1KTIP.localdomain>
2022-03-03 17:46:10 +0100

    Find all products of a brand method

--- server/server.js
+++ server/server.js
@@ -5,27 +5,56 @@ const MONGODB_DB_NAME = 'products';
 
 async function start() {
     
-    const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
-    const db =  client.db(MONGODB_DB_NAME)
+  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+  const db =  client.db(MONGODB_DB_NAME)
 
-    const collection = db.collection('products');
+  const collection = db.collection('products');
 
-    //console.log(collection);
-    /*Insert the products in the database
-    const products= require('./products.json');
-    const result = collection.insertMany(products);
-    console.log(result);
-    
-*/
-  find_Brand(collection, 'Adresse Paris');
-  
+  //Insert_Products(collection)
+  //find_Brand(collection, 'Adresse Paris');
+  //find_filter_price(collection, 0 , 50);
+  //sorted_by_price(collection)
+  sorted_by_date(collection);
+  /*
+  const products = await collection.find().toArray();
+  console.log(products);*/
 }
 start()
 
+async function insert_Products(collection){
+  
+  //Insert the products in the database
+  const products= require('./products.json');
+  const result = collection.insertMany(products);  
+}
+
+//Displays only the products of a certain brand
 async function find_Brand(collection, brand){
-  const products = await collection.find({brand}).toArray();;
+  const products = await collection.find({brand}).toArray();
 
   console.log(products);
 }
+//Displays only the products between a specific price range
+async function find_filter_price(collection, min, max){
+  const products = await collection.find().toArray();
+  products.forEach(element => {  
+    if(element.price > min & element.price < max)  {
+      console.log(element);
+    }
+  });
+}
 
+async function sorted_by_price(collection){
+  const products = await collection.find().toArray();
+  const products_sorted = products.sort(function(a, b){return a.price - b.price});
+  
+  console.log(products_sorted)
+}
+
+async function sorted_by_date(collection){
+  const products = await collection.find().toArray();
+  const products_sorted = products.sort(function(a, b){return new Date(b.released_Date) - new Date(a.released_Date)});
+  
+  console.log(products_sorted)
+}
 
