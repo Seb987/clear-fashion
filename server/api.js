c82b4e835d8cd82ce699cdda0aa4876c2450e4ac
Seb987 <liaosebastien@DESKTOP-IQ1KTIP.localdomain>
2022-03-12 10:31:47 +0100

    Add api filter by brand & price

--- server/api.js
+++ server/api.js
@@ -1,7 +1,10 @@
 const cors = require('cors');
 const express = require('express');
+const req = require('express/lib/request');
 const helmet = require('helmet');
 const db = require('./server.js')
+const bodyParser = require('body-parser');
+const { query } = require('express');
 
 const PORT = 8092;
 
@@ -9,7 +12,8 @@ const app = express();
 
 module.exports = app;
 
-app.use(require('body-parser').json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 
@@ -18,14 +22,23 @@ app.options('*', cors());
 app.get('/', (request, response) => {
   response.send({'ack': true});
 });
-
+/*
 app.get('/products/:id', async(request, response) => {
+  console.log("id")
   response.send(await db.find({"_id":request.params.id}));
+});*/
+app.get('/products/search?', async(request, response) => {
+  response.send(await db.find({'brand': {$eq:request.query.brand},'price':{$lt:parseInt(request.query.price)}}));
 });
 
-app.get('/products/search', (request, response) => {
-  response.send({'blaack': false});
-});
+/*
+
+app.get('/products/search?limit=:limit/?brand=:brand/?price=:price', async(request, response) => {
+  console.log("search")
+  response.send(await db.find({'brand': {$eq:request.params.brand},'price':{$lt:request.params.price}}).limit(request.params.limit));
+});*/
+
+
 
 app.listen(PORT);
 
