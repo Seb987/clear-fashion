f71e7c1bea1f8a6b1721058263fbd336d12cdb0f
Sebastien Liao <liao.sebastien@gmail.com>
2022-03-14 12:53:45 +0100

    Modif api products/search

--- server/api.js
+++ server/api.js
@@ -13,7 +13,7 @@ const app = express();
 module.exports = app;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 
@@ -28,7 +28,22 @@ app.get('/products/:id', async(request, response) => {
   response.send(await db.find({"_id":request.params.id}));
 });*/
 app.get('/products/search?', async(request, response) => {
-  response.send(await db.find({'brand': {$eq:request.query.brand},'price':{$lt:parseInt(request.query.price)}}));
+  /*let query ={};
+  if(request.query.brand == undefined){
+    query={price:{$lt:request.query.price}}
+  }
+  else if(request.query.price == undefined){
+    query={brand:request.query.brand}
+  }
+  else {
+    query=}
+  }*/
+  //,{limit:request.query.limit}
+  console.log(request.query.brand)
+  console.log(request.query.price)
+  console.log(request.query.limit)
+  const result = await db.find({brand:request.query.brand, price:{$lt:request.query.price}});
+  response.send({"limit":request.query.limit,"total":result.length,"results":result});
 });
 
 /*
