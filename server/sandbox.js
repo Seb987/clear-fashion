8e92584b9d1555d8af14b47b1d44edf57ae59b33
Seb987 <liaosebastien@DESKTOP-IQ1KTIP.localdomain>
2022-03-19 11:07:06 +0100

    Modif sandbox.js

--- server/sandbox.js
+++ server/sandbox.js
@@ -36,7 +36,8 @@ async function sandbox (eshop) {
 
     */
     //const result = await db.insert(products_loom);
-    const loomOnly = await db.find({'brand':'loom','price':{$lt:30}});
+    const data= await db.getDB()
+    const loomOnly = await db.find({'brand':'loom','price':{$lt:30}},data);
     console.log(loomOnly)
     db.close();
 
